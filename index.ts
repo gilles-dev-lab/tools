29d48ff4058ccb794185daee367c1f92de329700
gilles-dev-lab <dev@gillesmaupoint.fr>
2025-12-14 12:37:08 +0100

    Update index.ts
    Exemples d'utilisation de Symbol

--- index.ts
+++ index.ts
@@ -11,3 +11,26 @@ function canDisplayDiscount(age: number): boolean {
     }
     return false;
 }
+
+// Symbol testing
+const price = {
+  amount: 99.99,
+  currency: "USD",
+  [Symbol.toPrimitive](hint: string) {
+    if (hint === "string") return `${this.currency} ${this.amount}`;
+    if (hint === "number") return this.amount;
+    return null;
+  },
+};
+
+console.log(String(price));
+console.log(Number(price));
+console.log(Number(price) + 10);
+
+const customReplacer = {
+  [Symbol.replace](string: string, replacement: string) {
+    return string.replace("apple", replacement); 
+  }
+};
+
+console.log("I like apple too much".replace(customReplacer, "banana")); 
