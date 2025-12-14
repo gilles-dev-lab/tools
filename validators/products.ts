73f64003419bdab5c2e72984d27dbf8748c2a835
gilles-dev-lab <dev@gillesmaupoint.fr>
2025-12-14 13:39:42 +0100

    Update products.ts

--- validators/products.ts
+++ validators/products.ts
@@ -1,6 +1,6 @@
 import { CodeProduct } from '../types/branded';
 
-export function assertIsCodeProductValide(codeProduit: string): codeProduit age is CodeProduct {
+export function assertIsCodeProductValide(codeProduit: string): asserts codeProduit is CodeProduct {
   if (codeProduit.lenght < 4) {
     throw new Error("Ce n'est pas un code produit");
   }
